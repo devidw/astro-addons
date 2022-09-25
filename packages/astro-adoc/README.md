00f3c9bda07bb9988945a61ff7a0c38e66cd1329
David Wolf <68775926+devidw@users.noreply.github.com>
2022-09-25 21:43:48 +0000

    update 10 files

--- packages/astro-adoc/README.md
+++ packages/astro-adoc/README.md
@@ -1,3 +1,30 @@
 # astro-adoc
 
 Integrate AsciiDoctor content into your Astro project.
+
+## Usage
+
+For example, to get the following routes:
+
+| Directory | URL
+| --- | ---
+| `src/articles/post.adoc` | `/blog/post`
+| `src/articles/sub/index.adoc` | `/blog/sub`
+| `src/articles/dig/deeper.adoc` | `/blog/dig/deeper`
+
+Your would add the following to `src/pages/blog/[...slug].astro`:
+
+```astro
+---
+import { getAdocPaths } from "astro-adoc"
+import { AsciiDoctor } from "astro-adoc/components"
+
+export async function getStaticPaths() {
+  return await getAdocPaths("src/articles", "slug")
+}
+
+const { slug } = Astro.params
+---
+
+<AsciiDoctor path={`src/articles/${slug}`} />
+```
--- packages/astro-adoc/src/utils.ts
+++ packages/astro-adoc/src/utils.ts
@@ -5,30 +5,29 @@ import { promises as fs } from 'fs'
 import { join } from 'path'
 
 export async function getFiles(dir: string): Promise<string[]> {
-    const dirents = await fs.readdir(dir, { withFileTypes: true })
-    const files = await Promise.all(dirents.map((dirent) => {
-        const res = join(dir, dirent.name)
-        return dirent.isDirectory() ? getFiles(res) : res
-    }))
-    return Array.prototype.concat(...files)
+  const dirents = await fs.readdir(dir, { withFileTypes: true })
+  const files = await Promise.all(dirents.map((dirent) => {
+    const res = join(dir, dirent.name)
+    return dirent.isDirectory() ? getFiles(res) : res
+  }))
+  return Array.prototype.concat(...files)
 }
 
-export async function getAdocPaths(dir: string, paramName: string = "slug"): Promise<{ params: { [key: string]: string } }[]> {
-    // maybe add trailing slash if not present
-    if (!dir.endsWith('/')) {
-        dir = dir + '/'
-    }
-    const files = await getFiles(dir)
-    const adocFiles = files.filter((file) => file.endsWith('.adoc'))
-    return adocFiles.map((file) => {
-        let path = file.replace('.adoc', '')
+export async function getAdocPaths(dir: string, paramName = 'slug'): Promise<{ params: { [key: string]: string } }[]> {
+  // maybe add trailing slash if not present
+  if (!dir.endsWith('/'))
+    dir = `${dir}/`
 
-        if (path.endsWith('/index')) {
-            path = path.replace('/index', '')
-        }
+  const files = await getFiles(dir)
+  const adocFiles = files.filter(file => file.endsWith('.adoc'))
+  return adocFiles.map((file) => {
+    let path = file.replace('.adoc', '')
 
-        path = path.replace(dir, '')
+    if (path.endsWith('/index'))
+      path = path.replace('/index', '')
 
-        return { params: { [paramName]: path } }
-    })
+    path = path.replace(dir, '')
+
+    return { params: { [paramName]: path } }
+  })
 }
