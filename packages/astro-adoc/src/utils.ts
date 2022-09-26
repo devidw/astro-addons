492c0020864a5434f0b295aabcd5cbdaa151179e
David Wolf <68775926+devidw@users.noreply.github.com>
2022-09-26 07:05:35 +0000

    update 5 files

--- packages/astro-adoc/src/utils.ts
+++ packages/astro-adoc/src/utils.ts
@@ -1,11 +1,14 @@
+
+import Processor from "asciidoctor"
+import type { Asciidoctor } from "asciidoctor"
+import * as fs from 'fs'
+import { join } from 'path'
+
 /**
  * Recursively get all .adoc files in a given directory
  */
-import { promises as fs } from 'fs'
-import { join } from 'path'
-
 export async function getFiles(dir: string): Promise<string[]> {
-  const dirents = await fs.readdir(dir, { withFileTypes: true })
+  const dirents = await fs.promises.readdir(dir, { withFileTypes: true })
   const files = await Promise.all(dirents.map((dirent) => {
     const res = join(dir, dirent.name)
     return dirent.isDirectory() ? getFiles(res) : res
@@ -30,4 +33,27 @@ export async function getAdocPaths(dir: string, paramName = 'slug'): Promise<{ p
 
     return { params: { [paramName]: path } }
   })
+}
+
+/**
+ * 
+ * @see https://asciidoctor.github.io/asciidoctor.js/2.2.5/#document
+ */
+export function getAdoc(path: string, options: Asciidoctor.ProcessorOptions = { safe: "safe" }) {
+  if (fs.existsSync(`${path}.adoc`)) {
+    path = `${path}.adoc`
+  } else if (fs.existsSync(`${path}/index.adoc`)) {
+    path = `${path}/index.adoc`
+  } else {
+    throw new Error(`No AsciiDoc file found at ${path}`)
+  }
+
+  try {
+    const processor = Processor()
+    const adoc = processor.loadFile(path, options)
+    return adoc
+  } catch (e) {
+    console.error(e)
+    throw new Error(`Failed to load AsciiDoc file at ${path}`)
+  }
 }
